00e4b22410dd1d837f171d62c38ab3e586faa4c7
Edouard <edouard.gravez@laposte.net>
2016-04-20 15:49:44 +0200

    Formulaire fonctionnant sur BDD distante

--- formulaire/login.php
+++ formulaire/login.php
@@ -1,15 +1,23 @@
 <html>
-    <head><title>Bienvenue sur Julie's School</title></head>
-    <body>
-        <h1>Bienvenue sur Julie's School</h1>
-        <h2>veuillez vous inscrire :</h2>
-        <form name="inscription" method="post" action="insertion_parent.php">
-            Entrez votre nom : <input type="text" name="lastname"/> <br/>
-            Entrez votre prénom : <input type="text" name="firstname"/><br/>
-            Entrez votre E mail : <input type="text" name="email"/><br/>
 
+<head>
+  <title>Bienvenue sur Julie's School</title>
+</head>
 
-            <input type="submit" name="valider" value="OK"/>
-        </form>
-    </body>
+<body>
+  <h1>Bienvenue sur Julie's School</h1>
+  <h2>veuillez vous inscrire :</h2>
+  <form name="inscription" method="post" action="insertion_parent.php">
+    Entrez votre nom :
+    <input type="text" name="lastname" id="lastname"/>
+    <br/> Entrez votre prénom :
+    <input type="text" name="firstname" id="firstname"/>
+    <br/> Entrez votre E mail :
+    <input type="text" name="email" id="email"/>
+    <br/>
+    
+    <input type="submit" name="valider" value="OK" />
+  </form>
+</body>
+
 </html>

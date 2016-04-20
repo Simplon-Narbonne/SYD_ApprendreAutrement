e0674fb6bf284edfb156aaef269da0eed45a019b
Edouard <edouard.gravez@laposte.net>
2016-04-20 22:26:37 +0200

    Ajout du H1 centré

--- formulaire/login.php
+++ formulaire/login.php
@@ -1,34 +1,4 @@
-<!-- <html>
-    <head>
-      <title>Bienvenue sur Julie's School</title>
-    </head>
-    <body>
-        <h1>Bienvenue sur Julie's School</h1>
-        <div class="container">
-        <div class="row" id="row.titrForm">
-            <div id="titrForm" class="col-xs-12 col-sm-9 col-md-9 col-lg-7 col-lg-offset-1">
-            <h2>veuillez vous inscrire :</h2>
-            </div>
-        </div>
-        </div>
 
-
-        <form name="inscription" method="post" action="insertion_parent.php">
-            Entrez votre nom : <input type="text" name="lastname"/> <br/>
-            Entrez votre prénom : <input type="text" name="firstname"/><br/>
-            Entrez votre E mail : <input type="text" name="email"/><br/>
-
-
-            <input type="submit" name="valider" value="OK"/>
-            <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
-
-            <link rel="stylesheet" href="assets/jquery-ui-custom/jquery.min.js" media="screen" title="no title" charset="utf-8">
-
-            <link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
-
-        </form>
-    </body>
-</html> -->
 <html lang="fr">
   <head>
     <meta charset="utf-8">
@@ -45,6 +15,7 @@
   </head>
 
   <body>
+    <h1>Bienvenue sur Julie's School</h1>
 
     <div class="container"> <!--  Début de corps de la plage -->
 
@@ -52,7 +23,7 @@
 
                   <!--  Titre formulaire -->
                 <div id="titrForm"class="col-md-4 col-md-offset-4">Formulaire d'inscription</div>
-                  <!--  Titre formulaire -->
+                  <!--  Champs formulaire -->
                 <div id="InputForm"class="col-md-8 col-md-offset-2">
                   <form name="inscription" method="post" action="insertion_parent.php">
                       Entrez votre nom : <input type="text" name="lastname"/> <br/>

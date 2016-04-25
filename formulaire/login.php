42771dd9332afd75144090b73157ba5ccf851d04
Edouard <edouard.gravez@laposte.net>
2016-04-25 22:17:44 +0200

    Débrousaillage de l'ancien code commenté, ça fait pas propre :-D

--- formulaire/login.php
+++ formulaire/login.php
@@ -1,34 +1,4 @@
-<!-- <html>
-<head>
-<title>Bienvenue sur Julie's School</title>
-</head>
-<body>
-<h1>Bienvenue sur Julie's School</h1>
-<div class="container">
-<div class="row" id="row.titrForm">
-<div id="titrForm" class="col-xs-12 col-sm-9 col-md-9 col-lg-7 col-lg-offset-1">
-<h2>veuillez vous inscrire :</h2>
-</div>
-</div>
-</div>
-
-
-<form name="inscription" method="post" action="insertion_parent.php">
-Entrez votre nom : <input type="text" name="lastname"/> <br/>
-Entrez votre prénom : <input type="text" name="firstname"/><br/>
-Entrez votre E mail : <input type="text" name="email"/><br/>
-
-
-<input type="submit" name="valider" value="OK"/>
-<link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
-
-<link rel="stylesheet" href="assets/jquery-ui-custom/jquery.min.js" media="screen" title="no title" charset="utf-8">
-
-<link rel="stylesheet" href="assets/css/style.css" media="screen" title="no title" charset="utf-8">
-
-</form>
-</body>
-</html> -->
+<!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
